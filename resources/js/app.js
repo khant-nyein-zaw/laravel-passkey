c41b42d0eb1b0978898080f84b830eeb5b14cb35
Khant Nyein Zaw <khantnyeinzaw6@gmail.com>
2024-10-17 02:52:11 +0630

    chore: publish telescope assets

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,21 +1,22 @@
-import axios from 'axios';
-import './bootstrap';
+import axios from "axios";
+import { startRegistration } from "@simplewebauthn/browser";
+import "./bootstrap";
 
-import Alpine from 'alpinejs';
+import Alpine from "alpinejs";
 
 window.Alpine = Alpine;
 
-document.addEventListener('alpine:init', () => {
-    Alpine.data('registerPasskey', () => ({
-        token: "2|kYGPxERFVdek52F2rU9z28fR2iAeguvZtSd1LApu62204efa",
+document.addEventListener("alpine:init", () => {
+    Alpine.data("registerPasskey", () => ({
         async register() {
-            const options = await axios.get('api/passkeys/register', {
-                headers: {
-                   Authorization: 'Bearer ' + this.token
-                }
-             });
-            console.log(options);
-        }
+            try {
+                const options = await axios.get("api/passkeys/register");
+                const passkey = await startRegistration(options.data);
+                console.log(passkey);
+            } catch (e) {
+                throw e;
+            }
+        },
     }));
 });
 
